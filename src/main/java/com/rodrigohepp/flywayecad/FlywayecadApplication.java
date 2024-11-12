ce15e07bb141e8323ee9d2fe086262453c339364
Rodrigo Joel Hepp <116319702+RodrigoHepp@users.noreply.github.com>
2024-11-12 17:53:12 -0300

    first commit

---
+++ src/main/java/com/rodrigohepp/flywayecad/FlywayecadApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigohepp.flywayecad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlywayecadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlywayecadApplication.class, args);
+	}
+
+}
